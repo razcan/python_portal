c9c8f619b146fd5bfdf82823087e9b9e9c689716
razvan mustata <razvan.mustata@gmail.com>
2020-12-16 15:35:33 +0200

    added payments

--- portal/payments/views.py
+++ portal/payments/views.py
@@ -1,8 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.db import connection
+
 
 def index(request):
     return render(request, 'payments_page.html')
 
+
 def payments(request):
-    return render(request, 'payments_page.html')
+    results = query_to_dicts("""        
+select top 100 DocumentId, SiteId, DocumentNumber, PaymentValue, PaymentDate,PartnerName from vwPayment
+        """)
+    return render(request, 'payments_page.html', {'results': results})
+
+
+def query_to_dicts(query_string, *query_args):
+    cursor = connection.cursor()
+    cursor.execute(query_string, query_args)
+    col_names = [desc[0] for desc in cursor.description]
+    while True:
+        row = cursor.fetchone()
+        if row is None:
+            break
+        row_dict = dict(zip(col_names, row))
+        print(row_dict)
+        yield row_dict
+    return
